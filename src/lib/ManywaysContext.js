64849b0599898729844bd21c6ac3461267f6dc44
Arjuna Shankar <contact@arjunashankar.com>
2024-01-16 13:56:14 -0500

    base

--- src/lib/ManywaysContext.js
+++ src/lib/ManywaysContext.js
@@ -1,10 +1,94 @@
-import { useContext, createContext } from "react";
-
+import { useContext, createContext, useEffect, useState } from "react";
+import NodeRenderer from "./NodeRenderer";
 const ManywaysContext = createContext(null);
 
-const ManywaysProvider = ({ children }) => {
+const ManywaysProvider = ({ children, slug, classNamePrefix = "mw" }) => {
+  let [nodes, setNodes] = useState([]);
+  let [responseId, setResponseId] = useState(false);
+  let [treeConfig, setTreeConfig] = useState({});
+  let [currentNodeId, setCurrentNodeId] = useState(false);
+  let [responses, setResponses] = useState([]);
+  let currentNode =
+    setCurrentNodeId !== false
+      ? nodes.find((n) => n.id === currentNodeId)
+      : false;
+
+  const getInitialData = () => {
+    fetch(`https://apiv2.manyways.io/response_sessions/${slug}/begin`)
+      .then((response) => response.json())
+      .then((data) => {
+        setNodes([data?.current_node]);
+        setCurrentNodeId(data?.node_id);
+        setResponseId(data?.id);
+      });
+  };
+
+  const goForward = async ({ formData }) => {
+    let theResponse = {
+      node_id: currentNode?.id,
+      response: formData,
+    };
+
+    await fetch(`https://apiv2.manyways.io/response_sessions/${responseId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(theResponse),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setNodes([...nodes, data]);
+        setResponses([...responses, theResponse]);
+        setCurrentNodeId(data?.id);
+      });
+  };
+
+  const goBack = async () => {
+    let theLastResponse = responses[responses.length - 1];
+    if (!!theLastResponse && !!theLastResponse?.node_id) {
+      setCurrentNodeId(theLastResponse?.node_id);
+    } else {
+      console.log("cannot go back");
+    }
+  };
+
+  useEffect(() => {
+    console.log("init");
+    getInitialData();
+  }, [slug]);
+
+  let getResponseByNodeID,
+    journeyNodes,
+    locale,
+    setLocale,
+    shareJourney,
+    copyLink;
   return (
-    <ManywaysContext.Provider value={{}}>{children}</ManywaysContext.Provider>
+    <ManywaysContext.Provider
+      value={{
+        nodes,
+        currentNodeId,
+        currentNode,
+        goBack,
+        responses,
+        goForward,
+        getResponseByNodeID,
+        treeConfig,
+        journeyNodes,
+        locale,
+        setLocale,
+        shareJourney,
+        copyLink,
+        classNamePrefix,
+      }}
+    >
+      <div className={`${classNamePrefix}-${slug}`}>
+        <NodeRenderer />
+        {children}
+      </div>
+    </ManywaysContext.Provider>
   );
 };
 
--- src/lib/NodeRenderer.js
+++ src/lib/NodeRenderer.js
@@ -1,5 +1,48 @@
+import { useManyways } from "./ManywaysContext";
+import validator from "@rjsf/validator-ajv8";
+import Form from "@rjsf/core";
+
 const NodeRenderer = (props) => {
-  return <div>NODE</div>;
+  const {
+    nodes,
+    goBack,
+    responses,
+    classNamePrefix,
+    currentNodeId,
+    goForward,
+  } = useManyways();
+
+  return nodes.map((currentNode) => {
+    let theResponse = responses.find((r) => r.node_id === currentNode?.id);
+    return (
+      <Form
+        disabled={!!theResponse}
+        formData={theResponse?.response}
+        className={`${classNamePrefix}-form is-current-node-${
+          currentNodeId === currentNode?.id
+        } has-response-${!!theResponse}`}
+        key={currentNode?.id || 1123456789}
+        onSubmit={goForward}
+        schema={currentNode?.form_schema}
+        validator={validator}
+        uiSchema={!!currentNode?.ui_schema ? currentNode?.ui_schema : {}}
+      >
+        {currentNodeId}
+        <button
+          className={`${classNamePrefix}-back`}
+          onClick={(e) => {
+            e.preventDefault();
+            goBack();
+          }}
+        >
+          Back
+        </button>
+        <button className={`${classNamePrefix}-next`} type="submit">
+          Submit
+        </button>
+      </Form>
+    );
+  });
 };
 
 export default NodeRenderer;
