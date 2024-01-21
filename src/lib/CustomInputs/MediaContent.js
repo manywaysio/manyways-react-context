3631ab63a78e8c311c3be67b1ff158c40fd35c6a
Amanda Jericho <ajericho@gmail.com>
2024-01-21 16:24:46 -0500

    styles

--- src/lib/CustomInputs/MediaContent.js
+++ src/lib/CustomInputs/MediaContent.js
@@ -1,10 +1,19 @@
 const MediaContent = ({ schema, ...props }) => {
   return (
     <div className={`media-content ${schema?.customClassName}`}>
-      <div className="text-container" dangerouslySetInnerHTML={{ __html: schema?.text }} />
-      <div className='image-container'>
-        <img src={schema?.media} alt={schema?.mediaAlt ? schema?.mediaAlt : ''}/>
-      </div>
+      <div
+        className="text-container"
+        dangerouslySetInnerHTML={{ __html: schema?.text }}
+      />
+
+      {schema?.media && (
+        <div className="image-container">
+          <img
+            src={schema?.media}
+            alt={schema?.mediaAlt ? schema?.mediaAlt : ""}
+          />
+        </div>
+      )}
     </div>
   );
 };
