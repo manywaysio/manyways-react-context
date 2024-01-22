52417c99f11340581a4880991477fd7ce658b346
Arjuna Shankar <contact@arjunashankar.com>
2024-01-22 14:52:08 -0500

    stub restart

--- src/index.js
+++ src/index.js
@@ -10,6 +10,29 @@ root.render(
   </React.StrictMode>
 );
 
+window.manyways = {};
+window.manyways.restart = function () {
+  alert("should restart");
+};
+
+// var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+// var eventer = window[eventMethod];
+// var messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message";
+
+// eventer(messageEvent, function (e) {
+//   if (
+//     e.data === "manyways-remove-iframe" ||
+//     e.message === "manyways-remove-iframe"
+//   ) {
+//     const frames = Array.from(
+//       document.getElementsByClassName("manyways-iframe-container")
+//     );
+//     frames.forEach((frame) => {
+//       frame.remove();
+//     });
+//   }
+// });
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
