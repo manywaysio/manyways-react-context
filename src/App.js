725a43c86157b6f223ed1944f9e8e83b80e92e59
Amanda Jericho <ajericho@gmail.com>
2024-01-23 17:57:39 -0500

    split App.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import "./styles/variables.css";
 import "./styles/App.css";
+import "./styles/layouts.css";
+import "./styles/buttons.css";
+import "./styles/media.css";
+import "./styles/forms.css";
+import "./styles/loader.css";
+
 import "./styles/mesca2.css";
+
 import { ManywaysProvider } from "./lib/ManywaysContext";
 
 function App() {
