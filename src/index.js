05f2f7b0c7bfa0942bd2be79c056f5f045db32be
Arjuna Shankar <contact@arjunashankar.com>
2024-02-12 15:42:34 -0500

    force dep

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 // import reportWebVitals from "./reportWebVitals";
 
-console.log("MANYWAYS!");
+console.log("MANYWAYS! ");
 
 class ManywaysWrapper extends HTMLElement {
   connectedCallback() {
