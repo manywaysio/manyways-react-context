defdc207f24f7386b1449470cce1bb76b48acf08
Amanda Jericho <ajericho@gmail.com>
2024-02-12 14:26:27 -0500

    sass-to-stirng

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,26 @@
 module.exports = {
   webpack: {
     configure: {
+      module: {
+        rules: [
+          {
+            // test: /\.(css|scss)$/,
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: [
+              "sass-to-string",
+              {
+                loader: "sass-loader",
+                options: {
+                  sassOptions: {
+                    outputStyle: "compressed",
+                  },
+                },
+              },
+            ],
+          },
+        ],
+      },
       entry: "./src/index.js",
       output: {
         filename: "static/js/manyways.js",
--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import "./styles/variables.css";
-import "./styles/App.css";
-import "./styles/layouts.css";
-import "./styles/buttons.css";
-import "./styles/media.css";
-import "./styles/forms.css";
-import "./styles/loader.css";
-import "./styles/mesca2.css";
+import styles from "./sass/sample.scss";
+
+// import vars from "./styles/variables.css";
+// import appStyles from "./styles/App.css";
+// import layoutStyles from "./styles/layouts.css";
+// import buttonStyles from "./styles/buttons.css";
+// import mediaStyles from "./styles/media.css";
+// import formStyles from "./styles/forms.css";
+// import loaderStyles from "./styles/loader.css";
+// import mescaStyles from "./styles/mesca2.css";
 
 // import all styles as a var called css
 
 import { ManywaysProvider } from "./lib/ManywaysContext";
 
 function App() {
+
+  console.log(styles)
+  const stylesToString = ""
+  
+
   return (
     <ManywaysProvider slug="rebates" mode="scroll">
-      <style dangerouslySetInnerHTML={{ __html: `cssHereAsALiteral` }}></style>
+      <style dangerouslySetInnerHTML={{ __html: stylesToString }}></style>
     </ManywaysProvider>
   );
 }
