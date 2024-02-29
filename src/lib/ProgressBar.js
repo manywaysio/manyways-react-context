a950c0cca4fdf65acfb124e97c5a97c83e0c7fd7
keil martin <keil@dotfusion.com>
2024-02-29 14:06:22 -0500

    quick progress bar

--- src/lib/ProgressBar.js
+++ src/lib/ProgressBar.js
@@ -4,13 +4,106 @@ const ProgressBar = ({ currentNode }) => {
     <div className="progress-bar">
       <div
         className={`first-bar ${
-          currentNode === "when it comes to travel" ? "bar-filled" : ""
+          currentNode === "when it comes to travel" ||
+          currentNode === "relax and adventure" ||
+          currentNode === "dream getaway" ||
+          currentNode === "charlotte" ||
+          currentNode === "have you taken a cruise" ||
+          currentNode === "cruise lines" ||
+          currentNode === "do you get seasick" ||
+          currentNode === "type of cruise" ||
+          currentNode === "dont-worry" ||
+          currentNode === "parts of the world" ||
+          currentNode === "traveling with" ||
+          currentNode === "traveling how long" ||
+          currentNode === "which seasons" ||
+          currentNode === "results"
+            ? "bar-filled"
+            : ""
+            ? "bar-filled"
+            : ""
         }`}></div>
       <div className="progress-inner">
         <div
           className={`bar-item ${
-            currentNode === "relax and adventure" ? "bar-filled" : ""
+            currentNode === "relax and adventure" ||
+            currentNode === "dream getaway" ||
+            currentNode === "charlotte" ||
+            currentNode === "have you taken a cruise" ||
+            currentNode === "cruise lines" ||
+            currentNode === "do you get seasick" ||
+            currentNode === "type of cruise" ||
+            currentNode === "dont-worry" ||
+            currentNode === "parts of the world" ||
+            currentNode === "traveling with" ||
+            currentNode === "traveling how long" ||
+            currentNode === "which seasons" ||
+            currentNode === "results"
+              ? "bar-filled"
+              : ""
           }`}></div>
+        <div
+          className={`bar-item ${
+            currentNode === "have you taken a cruise" ||
+            currentNode === "cruise lines" ||
+            currentNode === "do you get seasick" ||
+            currentNode === "type of cruise" ||
+            currentNode === "dont-worry" ||
+            currentNode === "parts of the world" ||
+            currentNode === "traveling with" ||
+            currentNode === "traveling how long" ||
+            currentNode === "which seasons" ||
+            currentNode === "results"
+              ? "bar-filled"
+              : ""
+          }`}></div>
+        <div
+          className={`bar-item ${
+            currentNode === "type of cruise" ||
+            currentNode === "dont-worry" ||
+            currentNode === "parts of the world" ||
+            currentNode === "traveling with" ||
+            currentNode === "traveling how long" ||
+            currentNode === "which seasons" ||
+            currentNode === "results"
+              ? "bar-filled"
+              : ""
+          }`}></div>
+        <div
+          className={`bar-item ${
+            currentNode === "parts of the world" ||
+            currentNode === "traveling with" ||
+            currentNode === "traveling how long" ||
+            currentNode === "which seasons" ||
+            currentNode === "results"
+              ? "bar-filled"
+              : ""
+          }`}></div>
+        <div
+          className={`bar-item ${
+            currentNode === "traveling with" ||
+            currentNode === "traveling how long" ||
+            currentNode === "which seasons" ||
+            currentNode === "results"
+              ? "bar-filled"
+              : ""
+          }`}></div>
+        <div
+          className={`bar-item ${
+            currentNode === "traveling how long" ||
+            currentNode === "which seasons" ||
+            currentNode === "results"
+              ? "bar-filled"
+              : ""
+          }`}></div>
+        <div
+          className={`bar-item ${
+            currentNode === "which seasons" || currentNode === "results"
+              ? "bar-filled"
+              : ""
+          }`}></div>
+        <div
+          className={`last-bar ${currentNode === "results" ? "bar-filled" : ""}`}></div>
       </div>
     </div>
   );
