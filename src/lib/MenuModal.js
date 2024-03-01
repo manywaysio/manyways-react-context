7fbb35668a4324ea0db70981b655462b5cb2319f
keil martin <keil@dotfusion.com>
2024-03-01 16:54:37 -0500

    menu added

--- src/lib/MenuModal.js
+++ src/lib/MenuModal.js
@@ -1,7 +1,12 @@
 const MenuModal = ({ menuModalOpen }) => {
   return (
     <div className={`menu-modal-container ${menuModalOpen ? "show" : ""}`}>
-      This is the menu
+      <ul>
+        <li>Restart this guide</li>
+        <li>Copy link to continue later</li>
+        <li>Share this guide</li>
+        <li className="manyways ">Powered by Manyways</li>
+      </ul>
     </div>
   );
 };
--- src/lib/NextAndBack.js
+++ src/lib/NextAndBack.js
@@ -4,13 +4,13 @@ const NextAndBack = ({ currentNode = {}, className }) => {
   const { goBack, responses, classNamePrefix, mode, labels } = useManyways();
 
   let theResponse = responses.find((r) => r.node_id === currentNode?.id);
+  console.log(currentNode);
 
   return (
     <div
       className={`${className} next-and-back-hidden-${
         mode === "scroll" && !!theResponse
-      } ${classNamePrefix}-next-and-back`}
-    >
+      } ${classNamePrefix}-next-and-back`}>
       {/* <button
         className={`${classNamePrefix}-back`}
         onClick={(e) => {
@@ -21,7 +21,7 @@ const NextAndBack = ({ currentNode = {}, className }) => {
       {labels ? labels.back : 'Back'}
       </button> */}
       <button className={`${classNamePrefix}-next`} type="submit">
-        {labels ? labels.next : 'Next'}
+        {labels ? labels.next : "Next"}
       </button>
     </div>
   );
