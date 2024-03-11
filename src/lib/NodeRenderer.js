7185b0f53203d6b62fa2ff792b4a39cb7dc90b64
Arjuna Shankar <contact@arjunashankar.com>
2024-03-11 16:31:51 -0400

    scenes

--- src/lib/NodeRenderer.js
+++ src/lib/NodeRenderer.js
@@ -197,7 +197,7 @@ const NodeRenderer = (props) => {
               )}`}
             />
             <div className={`background-blur-${slugify(currentNode?.title)}`} />
-            {currentNode?.title?.toLowerCase() == "start" && <EPTResults />}
+            {currentNode?.title?.toLowerCase() == "results" && <EPTResults />}
             <div
               className={`${classNamePrefix}-container ${
                 currentNode?.title == "Parts of the world"
@@ -206,7 +206,7 @@ const NodeRenderer = (props) => {
               }`}
               style={{
                 display:
-                  currentNode?.title?.toLowerCase() == "start"
+                  currentNode?.title?.toLowerCase() == "results"
                     ? "none"
                     : "block",
               }}
