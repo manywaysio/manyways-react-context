19e3602056b425ac02e83d31fb210b5023eebdc6
keil martin <keil@dotfusion.com>
2024-03-17 15:59:59 -0400

    QA hover ticket

--- src/icons/SvgFacebook.js
+++ src/icons/SvgFacebook.js
@@ -8,7 +8,7 @@ const SvgFacebook = (props) => (
     fill="none"
     {...props}>
     <path
-      fill="#fff"
+      fill="currentColor"
       d="M2.75 7.333H0V11h2.75v11h4.583V11h3.3L11 7.333H7.333V5.775c0-.825.184-1.192 1.009-1.192H11V0H7.517C4.217 0 2.75 1.467 2.75 4.217v3.116Z"
     />
   </svg>
