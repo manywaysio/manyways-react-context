4dfbe56e69350aab5e073d3ad2f1704a6ffce83b
Amanda Jericho <ajericho@gmail.com>
2024-03-20 09:16:10 -0400

    enable search on inputs

--- src/lib/CustomInputs/ManywaysSelect.js
+++ src/lib/CustomInputs/ManywaysSelect.js
@@ -162,7 +162,7 @@ const ManywaysSelect = ({ value, onChange, disabled, ...props }) => {
         }}
         menuIsOpen={menuIsOpen}
         isDisabled={disabled}
-        isSearchable={false}
+        isSearchable={true}
         value={theValue}
         placeholder={props.placeholder}
         options={theOptions}
