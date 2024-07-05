7682a9cbf5e58028a67b18cf3d65ca5d27c20476
Amanda Jericho <ajericho@gmail.com>
2024-07-05 07:18:02 -0400

    fix last selected options

--- src/lib/ManywaysContext.js
+++ src/lib/ManywaysContext.js
@@ -89,8 +89,6 @@ const ManywaysProvider = ({
       return;
     }
 
-    console.log(ignoreContinue);
-
     if (shouldContinue() && !ignoreContinue) {
       const sessionId = window.location.search.split("continue=")[1];
       continueJourney(sessionId);
