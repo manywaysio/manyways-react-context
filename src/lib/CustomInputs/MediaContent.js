f62b81a3eeb29647f987a60e1e180feabc51f1fd
Arjuna Shankar <contact@arjunashankar.com>
2024-09-27 18:07:48 -0400

    fix the bix

--- src/lib/CustomInputs/MediaContent.js
+++ src/lib/CustomInputs/MediaContent.js
@@ -1,10 +1,36 @@
 const MediaContent = ({ schema, ...props }) => {
   console.log("text", schema.text);
+  // find tag of <indoor></indoor> and relace contents with IndoorUnitWidget
+  // find tag of <outdoor></outdoor> and remove spaces at the end of the content
+  const cleanUp = (str) => {
+    let o = str
+      .replace(/<indoor>/, "")
+      .replace(/<\/indoor>/, "")
+      .replace(/<outdoor>/, "")
+      .replace(/<\/outdoor>/, "");
+    // remove all * from the end of the string
+    let clean = o.replace(/\*+$/, "");
+    // remove all numbers from the end of the strint
+    clean = clean.replace(/\d+$/, "");
+    // remove all dashes from the string
+    clean = clean.replace(/-/g, "");
+    // remova all instances of U at the end of the string
+    clean = clean.replace(/U+$/, "");
+    return clean;
+  };
+
+  let replacedText = schema.text.replace(
+    /<indoor>.*<\/indoor>/,
+    function (match, capture) {
+      return cleanUp(match);
+    }
+  );
+
   return (
     <div className={`media-content ${schema?.customClassName}`}>
       <div
         className="text-container"
-        dangerouslySetInnerHTML={{ __html: schema?.text }}
+        dangerouslySetInnerHTML={{ __html: replacedText }}
       />
 
       {schema?.media && (
