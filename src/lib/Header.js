ef67dbba418edb006d49802c20b11669f781ce36
Amanda Jericho <ajericho@gmail.com>
2026-03-09 10:44:55 -0400

    with header button

--- src/lib/Header.js
+++ src/lib/Header.js
@@ -10,23 +10,31 @@ const Header = () => {
   // console.log(treeConfig);
 
   return (
-    <div>
-      <div
-        className={`mesca-header mesca-header-with-svg  ${classNamePrefix}-container`}
+    <div className={`${classNamePrefix}-container`}>
+      <a
+        href={
+          locale === "fr"
+            ? "https://www.mitsubishielectric.ca/fr/hvac/where-to-buy"
+            : "https://www.mitsubishielectric.ca/en/hvac/where-to-buy"
+        }
+        className="button where-to-buy-header"
       >
+        {locale === "fr" ? "Où Acheter" : "Where to buy"}
+      </a>
+      <div className={`mesca-header mesca-header-with-svg  `}>
         {/* SVG positioned absolutely */}
         <div className="mesca-svg-container">
           <img src={mescaGraphic} alt="Mesca graphic" className="mesca-svg" />
         </div>
 
         {/* Existing header content */}
-        {content?.logo && (
+        {/* {content?.logo && (
           <img
             className={`${classNamePrefix}-logo`}
             src={content?.logo}
             alt="logo"
           />
-        )}
+        )}*/}
         {content?.header && (
           <div
             className={`text-container ${classNamePrefix}-container`}
