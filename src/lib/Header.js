f25041f915e128521c36a1e2259ea6e6b7e77597
Amanda Jericho <ajericho@gmail.com>
2026-03-09 11:45:23 -0400

    where to buy form

--- src/lib/Header.js
+++ src/lib/Header.js
@@ -11,16 +11,46 @@ const Header = () => {
 
   return (
     <div className={`${classNamePrefix}-container `}>
-      <a
-        href={
+      <form
+        id="redirectForm"
+        target="_blank"
+        action={
           locale === "fr"
             ? "https://www.mitsubishielectric.ca/fr/hvac/where-to-buy"
             : "https://www.mitsubishielectric.ca/en/hvac/where-to-buy"
         }
-        className="button where-to-buy-header"
+        method="get"
+        className="where-to-buy where-to-buy-header"
       >
-        {locale === "fr" ? "Où Acheter" : "Where to buy"}
-      </a>
+        <label htmlFor="postal_code_header">
+          {locale === "fr" ? "Où Acheter" : "Where to buy"}
+        </label>
+        <div className="findDealerForm">
+          <input
+            style={{ display: "none" }}
+            id="commercial_header"
+            name="commercial"
+            value="residential"
+            type="text"
+            readOnly
+          />
+          <input
+            id="postal_code_header"
+            name="postal_code"
+            type="text"
+            pattern="[A-Za-z][0-9][A-Za-z] ?[0-9][A-Za-z][0-9]"
+            onInvalid={(e) =>
+              e.target.setCustomValidity("Please enter a valid postal code")
+            }
+            onChange={(e) => e.target.setCustomValidity("")}
+            required
+            placeholder={locale === "fr" ? "Code postal" : "Postal Code"}
+          />
+          <button type="submit">
+            {locale === "fr" ? "Trouvez un concessionnaire" : "Find a dealer"}
+          </button>
+        </div>
+      </form>
       <div className={`mesca-header mesca-header-with-svg  `}>
         {/* SVG positioned absolutely */}
         <div className="mesca-svg-container">
